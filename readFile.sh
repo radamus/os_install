b952d80b79cb2e57b5c2de7d24ebfbfca66afb2f
Radek Adamus <adamus.r@gmail.com>
2014-06-08 20:13:28 +0200

    reading array

--- readFile.sh
+++ readFile.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+declare -A passwords
 while read line
 do
     name=$line
     arrIN=(${name//=/ })
-    echo "Text read from file - $name"
-    for i in "${arrIN[@]}"
-    do
-    	echo $i
-    done
+    echo "Text read from line - $name"
+    echo "${arrIn[0]}"
+    echo "${arrIn[1]}"
+    #for i in "${arrIN[@]}"
+    #do
+    #	echo $i
+    #done
 done < $1
