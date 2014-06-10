29a63826aeac31b809f7ae5b0d0ba6e2d89f2183
radamus <adamus.r@gmail.com>
2014-06-10 06:55:42 +0200

    splitting into assoc array

--- readFile.sh
+++ readFile.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 declare -A passwords
+
 while read line
 do
     name=$line
     arrIN=(${name//=/ })
-    echo "Text read from line - $name"
-    echo "${arrIn[0]}"
-    echo "${arrIn[1]}"
+    passwords[$arrIN]=${arrIN[1]}
+#    echo "Text read from line - $name"
+#    echo ${arrIN[0]}
+#    echo "${arrIN[1]}"
     #for i in "${arrIN[@]}"
     #do
     #	echo $i
     #done
 done < $1
+
+for pass in "${!passwords[@]}"; do
+	echo "$pass -> ${passwords["$pass"]}"
+done
